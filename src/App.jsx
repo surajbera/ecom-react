93e480f7b2edc709f60efb6ab2b77905baac45bf
suraj.be <suraj.be@media.net>
2023-09-27 21:28:41 +0530

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Homepage from './components/Home/Homepage';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Homepage />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,12 +32,12 @@ const Navbar = () => {
 
   return (
     <header>
-      <nav className='bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-slate-600'>
+      <nav className='bg-white border-gray-200 px-4 py-2.5 dark:bg-slate-600'>
         <div className='flex flex-wrap items-center mx-auto max-w-screen-xl justify-between'>
           <div className='flex gap-4'>
             <a href='https://flowbite.com' className='flex items-center gap-1'>
               <span>
-                <BiSolidPurchaseTag size={24} />
+                <BiSolidPurchaseTag size={24} color='#1e40af' />
               </span>
               <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white text-light-700 dark:text-white'>
                 EcomReact
@@ -72,13 +72,13 @@ const Navbar = () => {
                 <input
                   type='search'
                   id='default-search'
-                  className='block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-light-500 focus:border-light-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark-500 dark:focus:border-dark-500'
+                  className='block w-full p-2 pl-9 pr-20 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-light-500 focus:border-light-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-dark-500 dark:focus:border-dark-500'
                   placeholder='Search Products...'
                   required=''
                 />
                 <button
                   type='submit'
-                  className='text-white absolute right-2.5 bottom-2.5 bg-light-700 hover:bg-light-800 focus:ring-4 focus:outline-none focus:ring-light-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-dark-600 dark:hover:bg-dark-700 dark:focus:ring-dark-800'
+                  className='text-white absolute bg-light-700 hover:bg-light-800 focus:ring-4 focus:outline-none focus:ring-light-300 font-medium rounded-lg text-xs px-3 py-1.5 dark:bg-dark-600 dark:hover:bg-dark-700 dark:focus:ring-dark-800 right-1.5 top-1/2 -translate-y-2/4'
                 >
                   Search
                 </button>
